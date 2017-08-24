44493799858255b3cfc9648cfb86046226fc6e1d
kork1 <koufodinos@gmail.com>
2017-08-25 00:45:31 +0300

    new file:   admin.php 	new file:   class.user.php 	new file:   dbconfig.php 	new file:   home.php 	new file:   login.php 	new file:   logout.php 	new file:   store_employee.php 	new file:   transit_hub.php

---
+++ home.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]> <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]> <html class="lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>SWEET HOME ALABAHMA</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <section class="container" >
+    <div class="ADMIN SHIET THIS IS" align="center">
+      <h1>SWEET HOME ALABAMAAA </h1>
+            <h2>shit shit shit hot shit hot hot </h1>
+
+    </div>
+
+  </section>
+
+ </body>
+ </html>
