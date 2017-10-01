19d6f1201968b348f4ea4ce20edb5138bbeef4b6
ubuntu <dinoshotmail@yahoo.gr>
2017-10-01 06:32:23 +0300

    ze la new db - la polishe

--- thecmp.sql
+++ thecmp.sql
@@ -19,95 +19,97 @@ create table login(
 	foreign key (id) references employees(id)
 	on update cascade on delete cascade
 	)Engine=InnoDB;
-create table TransitHubs(
-	TH_id int unsigned not null auto_increment,
+create table transit_hubs(
+	th_id int unsigned not null auto_increment,
 	name varchar(250) default'unset',
 	street varchar(250) default 'unset',
 	stnum int unsigned not null,
 	city varchar(250) default 'unset',
-	TK int unsigned not null,
+	tk int unsigned not null,
 	gpsc varchar(250) default 'unset',
-	primary key(TH_id)
+	primary key(th_id)
 	)Engine=InnoDB;
-create table LocalStores(
-	LS_id int unsigned not null auto_increment,
+create table local_stores(
+	ls_id int unsigned not null auto_increment,
 	name varchar(250) default'unset',
 	street varchar(250) default 'unset',
 	stnum int unsigned not null,
 	city varchar(250) default 'unset',
-	TK int unsigned not null,
-		#gpsc ?? 
-		gpsc varchar(250) default 'unset',
-		ServTH_id int unsigned,
-		primary key(LS_id),
-		foreign key (ServTH_id) references TransitHubs(TH_id)
-		on update cascade on delete cascade
+	tk int unsigned not null,
+	gpsc varchar(250) default 'unset',
+	th_id int unsigned,
+	primary key(ls_id),
+	foreign key (th_id) references transit_hubs(th_id)
+	on update cascade on delete cascade
 		)Engine=InnoDB;
-create table THemps(
-	THemp_id int unsigned not null,
+create table th_employees(
+	th_emp_id int unsigned not null,
 	#username varchar(250) default'unset',
 	#pass varchar(250) default 'unset',
-	Thub int unsigned,
-	primary key(THemp_id),
+	th_id int unsigned,
+	primary key(th_emp_id),
 	#unique(username),
-	foreign key (THemp_id) references employees(id)
+	foreign key (th_emp_id) references employees(id)
 	on update cascade on delete cascade,
-	foreign key (Thub) references TransitHubs(TH_id)
+	foreign key (th_id) references transit_hubs(th_id)
 	on update cascade on delete cascade
 	)Engine=InnoDB;
-create table LSemps(
-	LSemp_id int unsigned not null,
+create table ls_employees(
+	id int unsigned not null,
 	#username varchar(250) default'unset',
 	#pass varchar(250) default 'unset',
-	LStore int unsigned,
-	primary key(LSemp_id),
+	ls_id int unsigned,
+	primary key(id),
 	#unique(username),
-	foreign key (LSemp_id) references employees(id)
+	foreign key (id) references employees(id)
 	on update cascade on delete cascade,
-	foreign key (LStore) references LocalStores(LS_id)
+	foreign key (ls_id) references local_stores(ls_id)
 	on update cascade on delete cascade
 	)Engine=InnoDB;
-create table client_info(
-	cl_id int unsigned not null auto_increment,
-	name varchar(250) default'unknown',
-	surname varchar(250) default'unknown',
-	afm char(11) default '0',
-	phone int unsigned default '0',
-	primary key(cl_id)
-	)Engine=InnoDB;
-create table Shipment(
+
+-- create table client_info(
+-- 	cl_id int unsigned not null auto_increment,
+-- 	name varchar(250) default'unknown',
+-- 	surname varchar(250) default'unknown',
+-- 	afm char(11) default '0',
+-- 	phone int unsigned default '0',
+-- 	primary key(cl_id)
+-- 	)Engine=InnoDB;
+
+create table shipment(
 	tracknumber varchar(7) not null,
 	from_cl_id int unsigned not null,
-	to_client_id int unsigned not null,
+	to_cl_id int unsigned not null,
 	from_store int unsigned not null,
 	to_store int unsigned not null,
 	cretor_lse int unsigned not null,
 	express bit not null,
 	est_cost int unsigned not null,
 	eta int unsigned not null,
+	delivered bit,
 	primary key(tracknumber),
-	foreign key(from_cl_id) references client_info(cl_id)
+	-- foreign key(from_cl_id) references client_info(cl_id)
+	-- on update cascade on delete no action,
+	-- foreign key(to_cl_id) references client_info(cl_id)
+	-- on update cascade on delete no action,
+	foreign key(from_store) references local_stores(ls_id)
 	on update cascade on delete no action,
-	foreign key(to_client_id) references client_info(cl_id)
+	foreign key(to_store) references local_stores(ls_id)
 	on update cascade on delete no action,
-	foreign key(from_store) references LocalStores(LS_id)
-	on update cascade on delete no action,
-	foreign key(to_store) references LocalStores(LS_id)
-	on update cascade on delete no action,
-	foreign key(cretor_lse) references LSemps(LSemp_id)
+	foreign key(cretor_lse) references ls_employees(id)
 	on update cascade on delete no action 
 	)Engine=InnoDB;
-create table TrackHistory(
+create table trackhistory(
 	tracknumber varchar(7) not null,
-	the_scanner int unsigned,
+	th_emp_id int unsigned,
 	tstamp timestamp,
 	transitnode int unsigned not null,
 	past_through bit,
 	primary key(tracknumber,transitnode),
-	foreign key(tracknumber) references Shipment(tracknumber)
+	foreign key(tracknumber) references shipment(tracknumber)
 	on update cascade on delete no action,
-	foreign key(the_scanner) references THemps(THemp_id)
+	foreign key(th_emp_id) references th_employees(th_emp_id)
 	on update cascade on delete no action,
-	foreign key(transitnode) references TransitHubs(TH_id)
+	foreign key(transitnode) references transit_hubs(th_id)
 	on update cascade on delete no action 
 	)Engine=InnoDB;
