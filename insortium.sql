9189d52b6c5bc0d4a45425088d78d3abf28a86fb
ubuntu <dinoshotmail@yahoo.gr>
2017-10-02 04:58:52 +0300

    ze la new db - la polishe ze la le polishe polishe^2

--- insortium.sql
+++ insortium.sql
@@ -1,54 +1,84 @@
 # tha to evaza sto telos tou create script gia na diagrafei ta proigoumena kathe fora pou to trexeis yo.
-#ola ta auto increment ksekinane apo to miden
+#ola ta auto increment ksekinane apo to den ginetai alliws yo
 #insert into employees values ('','','','','');
 
-
-
 insert into employees values 
-	(0,NULL,NULL,'Pantelis','Kakomoiris'),
-	(NULL,NULL,NULL,'Maria','Tsoupwti'),
-	(NULL,NULL,NULL,'Antwnis','Fligkifakos'),
+	(NULL,NULL,NULL,'Pantelis','Kakomoiris'),
+	(NULL,NULL,NULL,'Marina','Mpliampla'),
+	(NULL,NULL,NULL,'Antwnis','Fligkiflogkis'),
 	(NULL,NULL,NULL,'Mpitsimplikis','Pfffatos'),
 	(NULL,NULL,NULL,'Fripifroupis','Fonias'),
 	(NULL,NULL,NULL,'Vigkos','Karavigkos');
 #admin -> 1
-# lse  -> 2
-# the  -> 3
+# the  -> 2
+# lse  -> 3
 #ta parakatw accounts se pliri isodunamia me ti seira apo panw
 insert into login values 
-	(0,'admin','admin','1'),
-	(1,'lse1','lse1','2'),#douleuei stin athina(aspropyrgos)
-	(2,'lse2','lse2','2'),#douleuei sti salonika
-	(3,'the1','the1','3'),#douleuei stin athina(aspropyrgos)
-	(4,'the1','the1','3'),#douleuei sti larissa
-	(5,'the2','the2','3');#douleuei sti mana sou astamatita kathe  mera(thesalonik)
+	(1,'admin','admin','1'),
+	(2,'lse1','lse1','3'),#douleuei stin athina(aspropyrgos)
+	(3,'lse2','lse2','3'),#douleuei sti salonika
+	(4,'the1','the1','2'),#douleuei stin athina(aspropyrgos)
+	(5,'the2','the2','2'),#douleuei sti larissa
+	(6,'the3','the3','2');#douleuei sti mana sou astamatita kathe  mera(thesalonik)
 
 #Beware for the ids(an ta allakseis prepei na allakseis kai sto local stores apo katw to teleutaio argument)
 #paliopapara den anevases ta arxeia sto github, pa-pa-ra
-insert into transit_hubs(th_id, name)values 
-	('0','Alexandroupoli'),
-	('1','Heraklion'),
-	('2','Aspropyrgos'),
-	('3','Patra'),
-	('4','Ioannina'),
-	('5','Thessaloniki'),
-	('6','Larisa'),
-	('7','Kalamata'),
-	('8','Mytilini');
+insert into transit_hubs(th_id,name)values 
+	(0,'Ioannina'),
+	(1,'Thessaloniki'),
+	(2,'Alexandroupoli'),
+	(3,'Larisa'),
+	(4,'Mytilini'),
+	(5,'Patra'),
+	(6,'Aspropyrgos'),
+	(7,'Kalamata'),
+	(9,'Heraklion');
+
+
+-- 	('Alexandroupoli'),
+-- 	('Heraklion'),
+-- 	('Aspropyrgos'),
+-- 	('Patra'),
+-- 	('Ioannina'),
+-- 	('Thessaloniki'),
+-- 	('Larisa'),
+-- 	('Kalamata'),
+-- 	('Mytilini');
+-- 	/* graph contents are
+-- 0: iwannina 
+-- 1: thessaloniki
+-- 2: aleksandroupoli
+-- 3: larissa
+-- 4: lesvos
+-- 5: patras
+-- 6: athina
+-- 7: kalamata
+-- 8: iraklion
+-- */
+
+-- NULL,
+-- NULL,
+-- NULL,
+-- NULL,
+-- NULL,
+-- NULL,
+-- NULL,
+-- NULL,
+-- NULL,
 
 insert into local_stores(ls_id, name, th_id) values 
-	(0,'Bleyk-Fleyk','5'),#sti thesaloniki
-	(NULL,'Make-Make','2');#stin athina(aspropyrgos)
+	(NULL,'Bleyk-Fleyk','6'),#sti thesaloniki
+	(NULL,'Make-Make','3');#stin athina(aspropyrgos)
 
 insert into th_employees values 
-	('3','2'),#poios empl se poio th 
-	('4','6'),
-	('5','5');
+	('3','3'),#poios empl se poio th 
+	('4','7'),
+	('5','6');
 
 
 insert into ls_employees values 
-	('1','1'),#poios empl se poio ls(o 1 sto ls 1 to opoio eksupureteitai apo to th 2(athina))
-	('2','0');
+	('1','2'),#poios empl se poio ls(o 1 sto ls 1 to opoio eksupureteitai apo to th 2(athina))
+	('2','1');
 
 
 # ta upoloipa einai i mama sou sta tessera
--- thecmp.sql
+++ thecmp.sql
@@ -23,13 +23,14 @@ create table login(
 	on update cascade on delete cascade
 	)Engine=InnoDB;
 create table transit_hubs(
-	th_id int unsigned not null auto_increment,
+	th_id int unsigned not null,
 	name varchar(250) default 'unset',
 	street varchar(250) default 'unset',
 	stnum int unsigned default 0,
 	city varchar(250) default 'unset',
 	tk int unsigned default 0,
-	gpsc varchar(250) default 'unset',
+	coord_x varchar(250) default 'unset',
+	coord_y varchar(250) default 'unset',
 	primary key(th_id)
 	)Engine=InnoDB;
 create table local_stores(
@@ -39,7 +40,8 @@ create table local_stores(
 	stnum int unsigned default 0,
 	city varchar(250) default 'unset',
 	tk int unsigned default 0,
-	gpsc varchar(250) default 'unset',
+	coord_x varchar(250) default 'unset',
+	coord_y varchar(250) default 'unset',
 	th_id int unsigned not null,
 	primary key(ls_id),
 
@@ -56,6 +58,7 @@ create table th_employees(
 	foreign key (th_id) references transit_hubs(th_id)
 	on update cascade on delete cascade
 	)Engine=InnoDB;
+
 create table ls_employees(
 	id int unsigned not null,
 	ls_id int unsigned,
@@ -109,44 +112,57 @@ create table trackhistory(
 
 insert into employees values 
 	(NULL,NULL,NULL,'Pantelis','Kakomoiris'),
-	(NULL,NULL,NULL,'Maria','Tsoupwti'),
-	(NULL,NULL,NULL,'Antwnis','Fligkifakos'),
+	(NULL,NULL,NULL,'Marina','Mpliampla'),
+	(NULL,NULL,NULL,'Antwnis','Fligkiflogkis'),
 	(NULL,NULL,NULL,'Mpitsimplikis','Pfffatos'),
 	(NULL,NULL,NULL,'Fripifroupis','Fonias'),
 	(NULL,NULL,NULL,'Vigkos','Karavigkos');
 #admin -> 1
-# lse  -> 2
-# the  -> 3
+# the  -> 2
+# lse  -> 3
 #ta parakatw accounts se pliri isodunamia me ti seira apo panw
 insert into login values 
 	(1,'admin','admin','1'),
-	(2,'lse1','lse1','2'),#douleuei stin athina(aspropyrgos)
-	(3,'lse2','lse2','2'),#douleuei sti salonika
-	(4,'the1','the1','3'),#douleuei stin athina(aspropyrgos)
-	(5,'the2','the2','3'),#douleuei sti larissa
-	(6,'the3','the3','3');#douleuei sti mana sou astamatita kathe  mera(thesalonik)
+	(2,'lse1','lse1','3'),#douleuei stin athina(aspropyrgos)
+	(3,'lse2','lse2','3'),#douleuei sti salonika
+	(4,'the1','the1','2'),#douleuei stin athina(aspropyrgos)
+	(5,'the2','the2','2'),#douleuei sti larissa
+	(6,'the3','the3','2');#douleuei sti mana sou astamatita kathe  mera(thesalonik)
 
 #Beware for the ids(an ta allakseis prepei na allakseis kai sto local stores apo katw to teleutaio argument)
 #paliopapara den anevases ta arxeia sto github, pa-pa-ra
-insert into transit_hubs(name)values 
-	('Alexandroupoli'),
-	('Heraklion'),
-	('Aspropyrgos'),
-	('Patra'),
-	('Ioannina'),
-	('Thessaloniki'),
-	('Larisa'),
-	('Kalamata'),
-	('Mytilini');
--- (,'Alexandroupoli'),
--- 	('1','Heraklion'),
--- 	('2','Aspropyrgos'),
--- 	('3','Patra'),
--- 	('4','Ioannina'),
--- 	('5','Thessaloniki'),
--- 	('6','Larisa'),
--- 	('7','Kalamata'),
--- 	('8','Mytilini');
+insert into transit_hubs(th_id,name)values 
+	(0,'Ioannina'),
+	(1,'Thessaloniki'),
+	(2,'Alexandroupoli'),
+	(3,'Larisa'),
+	(4,'Mytilini'),
+	(5,'Patra'),
+	(6,'Aspropyrgos'),
+	(7,'Kalamata'),
+	(9,'Heraklion');
+
+
+-- 	('Alexandroupoli'),
+-- 	('Heraklion'),
+-- 	('Aspropyrgos'),
+-- 	('Patra'),
+-- 	('Ioannina'),
+-- 	('Thessaloniki'),
+-- 	('Larisa'),
+-- 	('Kalamata'),
+-- 	('Mytilini');
+-- 	/* graph contents are
+-- 0: iwannina 
+-- 1: thessaloniki
+-- 2: aleksandroupoli
+-- 3: larissa
+-- 4: lesvos
+-- 5: patras
+-- 6: athina
+-- 7: kalamata
+-- 8: iraklion
+-- */
 
 -- NULL,
 -- NULL,
