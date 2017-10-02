4a7a2f63fcf68673c0e9fe83a2db526c16e1ecd3
kork1 <koufodinos@gmail.com>
2017-10-02 04:41:32 +0300

    salamoxtupi_last

---
+++ salamoxtupi/transit_hub.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]> <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]> <html class="lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>transit_hub_shit_this_is</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <section class="container" >
+    <div class="transit_hub_shit_this_is" align="center">
+      <h1>transit_hub_shit_this_is </h1>
+      
+    </div>
+
+  </section>
+
+ </body>
+ </html>
